0fbbea25a66debcfbc47a1eb6ca0cf520026b32e
Fabiano Oliveira <eu@fabiano.xyz>
2018-04-16 19:05:04 -0300

    Basic ext operation for Number types

---
+++ src/main/kotlin/xyz/fabiano/numberz/bigdecimal/Operations.kt
@@ -0,0 +1,4 @@
+package xyz.fabiano.numberz.bigdecimal
+
+class Operations {
+}
--- src/main/kotlin/xyz/fabiano/numberz/ext/NumberOperationsExtension.kt
+++ src/main/kotlin/xyz/fabiano/numberz/ext/NumberOperationsExtension.kt
@@ -1,2 +1,16 @@
 package xyz.fabiano.numberz.ext
 
+import xyz.fabiano.numberz.bigdecimal.BigDecimalOperations
+import java.math.BigDecimal
+
+fun <T : Number> Number.of(number: T): BigDecimal {
+    return BigDecimalOperations.asBigDecimal(number)
+}
+
+fun Number.standard(): BigDecimal {
+    return BigDecimalOperations.asBigDecimal(this)
+}
+
+infix fun BigDecimal.equalsIgnorePrecision(value: BigDecimal): Boolean {
+    return true
+}
